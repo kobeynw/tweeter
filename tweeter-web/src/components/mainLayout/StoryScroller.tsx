1a267da793592c532a9d09e3d10a80bea45bb84c
Kobey Workman <kobeyworkman1@gmail.com>
2025-01-28 14:08:09 -0700

    add components for part 1 of milestone 1

--- tweeter-web/src/components/mainLayout/StoryScroller.tsx
+++ tweeter-web/src/components/mainLayout/StoryScroller.tsx
@@ -1,11 +1,10 @@
 import { useContext } from "react";
 import { UserInfoContext } from "../userInfo/UserInfoProvider";
-import { AuthToken, FakeData, Status, User } from "tweeter-shared";
+import { AuthToken, FakeData, Status } from "tweeter-shared";
 import { useState, useEffect } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
-import { Link } from "react-router-dom";
-import Post from "../statusItem/Post";
 import useToastListener from "../toaster/ToastListenerHook";
+import StatusItem from "../statusItem/StatusItem";
 
 export const PAGE_SIZE = 10;
 
@@ -20,7 +19,7 @@ const StoryScroller = () => {
   const addItems = (newItems: Status[]) =>
     setNewItems(newItems);
 
-  const { displayedUser, setDisplayedUser, currentUser, authToken } =
+  const { displayedUser, authToken } =
     useContext(UserInfoContext);
 
   // Initialize the component whenever the displayed user changes
@@ -79,38 +78,6 @@ const StoryScroller = () => {
     // TODO: Replace with the result of calling server
     return FakeData.instance.getPageOfStatuses(lastItem, pageSize);
   };
-  const navigateToUser = async (event: React.MouseEvent): Promise<void> => {
-    event.preventDefault();
-
-    try {
-      const alias = extractAlias(event.target.toString());
-
-      const user = await getUser(authToken!, alias);
-
-      if (!!user) {
-        if (currentUser!.equals(user)) {
-          setDisplayedUser(currentUser!);
-        } else {
-          setDisplayedUser(user);
-        }
-      }
-    } catch (error) {
-      displayErrorMessage(`Failed to get user because of exception: ${error}`);
-    }
-  };
-
-  const extractAlias = (value: string): string => {
-    const index = value.indexOf("@");
-    return value.substring(index);
-  };
-
-  const getUser = async (
-    authToken: AuthToken,
-    alias: string
-  ): Promise<User | null> => {
-    // TODO: Replace with the result of calling server
-    return FakeData.instance.findUserByAlias(alias);
-  };
 
   return (
     <div className="container px-0 overflow-visible vh-100">
@@ -126,7 +93,8 @@ const StoryScroller = () => {
             key={index}
             className="row mb-3 mx-0 px-0 border rounded bg-white"
           >
-            <div className="col bg-light mx-0 px-0">
+            <StatusItem status={item} />
+            {/* <div className="col bg-light mx-0 px-0">
               <div className="container px-0">
                 <div className="row mx-0 px-0">
                   <div className="col-auto p-3">
@@ -156,7 +124,7 @@ const StoryScroller = () => {
                   </div>
                 </div>
               </div>
-            </div>
+            </div> */}
           </div>
         ))}
       </InfiniteScroll>
