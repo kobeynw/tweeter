b75911f70c0b1e57e85a2489f83265a9c0d39702
Kobey Workman <kobeyworkman1@gmail.com>
2025-02-14 09:28:12 -0700

    refactor Register Presenter to handle image files

--- tweeter-web/src/presenters/RegisterPresenter.ts
+++ tweeter-web/src/presenters/RegisterPresenter.ts
@@ -1,5 +1,6 @@
 import { UserService } from "../model/service/UserService";
 import { User, AuthToken } from "tweeter-shared";
+import { Buffer } from "buffer";
 
 export interface RegisterView {
   displayErrorMessage: (message: string) => void;
@@ -10,17 +11,20 @@ export interface RegisterView {
     rememberMe: boolean
   ) => void;
   navigate: (path: string) => void;
+  setImageBytes: React.Dispatch<React.SetStateAction<Uint8Array>>;
+  setImageUrl: React.Dispatch<React.SetStateAction<string>>;
+  setImageFileExtension: React.Dispatch<React.SetStateAction<string>>;
 }
 
 export class RegisterPresenter {
   private userService: UserService;
-  private _isLoading: boolean = false;
-  private _rememberMe: boolean = false;
+  private _isLoading: boolean;
   private _view: RegisterView;
 
   public constructor(view: RegisterView) {
     this.userService = new UserService();
     this._view = view;
+    this._isLoading = false;
   }
 
   async doRegister(
@@ -55,6 +59,42 @@ export class RegisterPresenter {
     }
   }
 
+  public handleImageFile(file: File | undefined) {
+    if (file) {
+      this.view.setImageUrl(URL.createObjectURL(file));
+
+      const reader = new FileReader();
+      reader.onload = (event: ProgressEvent<FileReader>) => {
+        const imageStringBase64 = event.target?.result as string;
+
+        // Remove unnecessary file metadata from the start of the string.
+        const imageStringBase64BufferContents =
+          imageStringBase64.split("base64,")[1];
+
+        const bytes: Uint8Array = Buffer.from(
+          imageStringBase64BufferContents,
+          "base64"
+        );
+
+        this.view.setImageBytes(bytes);
+      };
+      reader.readAsDataURL(file);
+
+      // Set image file extension (and move to a separate method)
+      const fileExtension = this.getFileExtension(file);
+      if (fileExtension) {
+        this.view.setImageFileExtension(fileExtension);
+      }
+    } else {
+      this.view.setImageUrl("");
+      this.view.setImageBytes(new Uint8Array());
+    }
+  }
+
+  private getFileExtension = (file: File): string | undefined => {
+    return file.name.split(".").pop();
+  };
+
   protected get view() {
     return this._view;
   }
@@ -65,13 +105,5 @@ export class RegisterPresenter {
 
   protected set isLoading(value: boolean) {
     this._isLoading = value;
-  }
-
-  public get rememberMe() {
-    return this._rememberMe;
-  }
-
-  public set rememberMe(value: boolean) {
-    this._rememberMe = value;
   }
 }
